7dbf74886076f0020c69c887c4ef304ee2097548
eric-harness <eric@harnessup.com>
2021-04-09 14:08:35 -0400

    feat: mvp for settings page to set client url and reset path

--- harness-inspections.php
+++ harness-inspections.php
@@ -32,7 +32,11 @@ add_action('plugins_loaded', function () {
     add_action('graphql_register_types', 'initGraphQLRegister');
     add_role('free_user', 'Free User', array('read' => true, 'level_0' => true));
 
-    add_filter('retrieve_password_message', function ($message, $key, $user_login, $user_data) {
+    add_filter('retrieve_password_message', function ($message, $key, $user_login, $user_data) { 
+      $options = get_option( 'harness_inspections_plugin_options' );
+      $client_url = (!empty($options['client_url'])) ? $options['client_url'] : getenv('CLIENT_URL');
+      $reset_path =  (!empty($options['password_reset'])) ? $options['password_reset'] : '/harness-inspection/password-reset/';
+
       $user_email = $user_data->user_email;
       $site_name = 'Harness Software Inspection App';
       $message   = __('Someone has requested a password reset for the following account:') . "\r\n\r\n";
@@ -43,7 +47,7 @@ add_action('plugins_loaded', function () {
       $message .= sprintf(__('Email Address: %s', 'Harness Software Inspection App'), $user_email) . "\r\n\r\n";
       $message .= __('If this was a mistake, ignore this email and nothing will happen.') . "\r\n\r\n";
       $message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
-      $message .= getenv('CLIENT_URL') . '/harness-inspection/password-reset/' . $key . "?email=" . rawurlencode($user_email) . "&username=" . rawurlencode($user_login) . "\r\n\r\n";
+      $message .= $client_url . $reset_path . $key . "?email=" . rawurlencode($user_email) . "&username=" . rawurlencode($user_login) . "\r\n\r\n";
       $requester_ip = $_SERVER['REMOTE_ADDR'];
       if ($requester_ip) {
         $message .= sprintf(
@@ -71,12 +75,66 @@ add_action('plugins_loaded', function () {
       }
     } 
 
-    add_filter( 'manage_edit-inspection-points_sortable_columns', 'inspections_realestate_sortable_columns');
-    function inspections_realestate_sortable_columns( $columns ) {
+    add_filter( 'manage_edit-inspection-points_sortable_columns', 'inspections_location_sortable_columns');
+    function inspections_location_sortable_columns( $columns ) {
       $columns['location'] = 'location_id';
       return $columns;
     }
   }
+
+  //SETTINGS PAGE
+  add_action( 'admin_menu', 'inspection_add_settings_page' );  
+
+  function inspection_add_settings_page() {
+    add_options_page( 'Harness Inspections Plugin Options', 'Harness Inspections', 'manage_options', 'harness-inspections', 'inspection_render_plugin_settings_page' );
+  }
+
+  function inspection_render_plugin_settings_page(){
+    if ( !current_user_can( 'manage_options' ) )  {
+      wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+    ?>
+      <div class="wrap">
+        <h2>Harness Inspection Plugin Settings</h2>
+        <form action="options.php" method="post">
+          <?php 
+            settings_fields( 'harness_inspections_plugin_options' );
+            do_settings_sections( 'harness_inspections_plugin' ); 
+          ?>
+            <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+          </div>
+    </form>
+      </div>
+    <?php
+  }
+
+  function harness_inspection_register_settings() {
+    register_setting( 'harness_inspections_plugin_options', 'harness_inspections_plugin_options', 'harness_inspections_plugin_options_validate' );
+    
+    add_settings_section( 'harness_inspections_settings', 'Harness Inspection Settings', 'harness_inspections_section_text', 'harness_inspections_plugin' );
+
+    add_settings_field( 'harness_inspections_plugin_setting_client_url', 'Client URL', 'harness_inspections_plugin_setting_client_url', 'harness_inspections_plugin', 'harness_inspections_settings' );
+
+    add_settings_field( 'harness_inspections_plugin_setting_password_reset', 'Password Reset Path', 'harness_inspections_plugin_setting_password_reset', 'harness_inspections_plugin', 'harness_inspections_settings' );
+
+}
+
+add_action( 'admin_init', 'harness_inspection_register_settings' );
+
+function harness_inspections_section_text() {
+  echo '<p>Here you can set all the options for the Harness Inspections plugin</p>';
+}
+
+function harness_inspections_plugin_setting_client_url() {
+  $options = get_option( 'harness_inspections_plugin_options' );
+  echo "<input id='harness_inspections_plugin_setting_client_url' name='harness_inspections_plugin_options[client_url]' type='text' value='" . esc_attr( $options['client_url'] ) . "' />";
+}
+
+function harness_inspections_plugin_setting_password_reset() {
+  $options = get_option( 'harness_inspections_plugin_options' );
+  echo "<input id='harness_inspections_plugin_setting_password_reset' name='harness_inspections_plugin_options[password_reset]' type='text' value='" . esc_attr( $options['password_reset'] ) . "' />";
+}
+
 }, 0);
 
 function init()
@@ -126,4 +184,3 @@ function show_admin_notice()
 {
   echo 'You are not meeting the requirements to use this plugin';
 }
-
